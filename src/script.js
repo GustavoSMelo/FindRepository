f19aa22f27e46f0aaedbe8c64c9622c5b762ea66
Gustavo Santos Melo <gsantos15569@gmail.com>
2019-11-01 00:16:20 -0300

    add a logic for increment a search of api-github for inside of html archive

--- src/script.js
+++ src/script.js
@@ -1,30 +1,75 @@
 "use strict";
 
 import '@babel/polyfill';
+import api from './api';
 
 class Aplication{
     constructor(){
         this.repositories = [];
-        this.refForm = window.document.querySelector('#repo-form'); //getElementById
+        this.refForm = window.document.querySelector('#repo-form'); 
+        this.refList = window.document.querySelector('#repo-list');
+        this.input = window.document.querySelector('#txtRepositorio');
         this.registerHandlers();
+        this.msgLoad = window.document.querySelector('#Carregamento');
     }
 
     registerHandlers(){
         this.refForm.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(){
-        event.preventDefault();
-        this.repositories.push({
-            nome: 'Gustavo S Melo',
-            link: 'https://github.com/GustavoSMelo',
-            avatar: 'https://avatars1.githubusercontent.com/u/45046288?s=400&u=19b6d6848332724f671d49fb2555400ba8effb69&v=4'
-        });
+    loading(load = true){
+        if(load == true){
+            this.msgLoad.innerHTML = `Carregando... aguarde`;
+        }
+        else{
+            this.msgLoad.innerHTML = ``;
+        }
+    }
 
-        console.log(this.repositories);
+    async addRepository(){
+        event.preventDefault();
+
+        const value =  this.input.value;
+
+        if(value == '' || value == null || value == undefined){
+            return;
+        }
+
+        this.loading();
+
+        try{
+            const response = await api.get(`repos/${value}`);
+            console.log(response);
+
+            const {name, owner: {avatar_url}, html_url} = response.data;
+            
+            this.repositories.push({
+                name,
+                html_url,
+                avatar_url
+            });
+
+            this.input.value = "";
+
+            console.log(this.repositories);
+        }
+        catch(Exception){window.alert('This repository doesnt exists');}
+        this.loading(false);
+
+        this.render();
+    }
+
+    render(){
+        this.refList.innerHTML = '';
+        this.repositories.forEach(item =>{
+            this.refList.innerHTML += `<li>
+                <img src ="${item.avatar_url}"/><br/>
+                <strong>${item.name}</strong><br/>
+                <a target="_blank" href="${item.html_url}">Acesse</a><br/>
+            </li>`;
+        });
     }
 }
 
 const app = new Aplication();
 app.registerHandlers();
-
